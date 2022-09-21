9d6512fa34695028abd32b44fc9c31493fec5322
Lilian <lilianslku@gmail.com>
2022-09-21 15:06:24 +0800

    added journal entries, changes to home page, and organized files to veto

---
+++ projects/php/header.php
@@ -0,0 +1,44 @@
+<!doctype html> 
+
+<html> 
+	<head> 
+		<title> 3-page Website </title>
+
+		<meta charset="utf-8">
+		<meta name="author" content="Lilian Habib">
+		<meta name="description" content="Welcome to my website!">
+
+		<meta name="viewport" content="width=device-width,initial-scale=1">
+	
+
+		<meta property="og:image" content="https://peprojects.dev/alpha-5/lilian/projects/3-page-website/images/hello.png">
+		
+
+		<link rel="shortcut icon" href="images/lilian1.png" type="image/png">
+
+		<link rel='stylesheet' href='styles.css'>
+		
+		<!-- font --> 
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&family=BhuTuka+Expanded+One&family=Give+You+Glory&family=Patrick+Hand&family=Prata&display=swap" rel="stylesheet">
+
+	
+		<!-- Facebook Meta Tags -->
+		  <meta property="og:url" content="https://peprojects.dev/alpha-5/lilian/">
+		  <meta property="og:type" content="website">
+		  <meta property="og:title" content=" index ">
+		  <meta property="og:description" content="A website containing all of Lilian's awesome websites created while in Perpetual Education - Design for the Web.">
+		  <meta property="og:image" content="https://peprojects.devimages/metalilian.png?hello">
+
+		  <!-- Twitter Meta Tags -->
+		  <meta name="twitter:card" content="summary_large_image">
+		  <meta property="twitter:domain" content="peprojects.dev">
+		  <meta property="twitter:url" content="https://peprojects.dev/alpha-5/lilian">
+		  <meta name="twitter:title" content=" index ">
+		  <meta name="twitter:description" content="A website containing all of Lilian's awesome websites created while in Perpetual Education - Design for the Web.">
+		  <meta name="twitter:image" content="https://peprojects.devimages/metalilian.png">
+
+	</head>
+
+	
