6ff4112f08c582ea5051c3a542ee68000b01d1a2
Lilian Habib <99775558+lilianhabib@users.noreply.github.com>
2022-09-26 16:52:16 -0700

    Add php-challenges files

---
+++ php-challenges/php-intro/footer.php
@@ -0,0 +1,9 @@
+
+
+
+
+<footer> 
+	<iframe src="https://lilianhabib.substack.com/embed" display="block" width= "100%" height="200" style="background: lightgrey;" frameborder="0" scrolling="no"></iframe>
+	</footer>
+	</body>
+</html>
