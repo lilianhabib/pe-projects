f696592c3a7f542dbdd328fe2bbfc230937a2552
Lilian <lilianslku@gmail.com>
2022-09-30 14:06:16 -0700

    Moved files to another folder

--- php-challenge/loops-iterations.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$fruitArray = ["Apple", "Banana", "Cherries", "Date", "Elderberry"]; //list of fruits in the array 
-	foreach ($fruitArray as $fruit) { // for each item in the fruit array, that is now called $fruit...
-		echo "<li>" . $fruit . "</li>"; // create a list of the $fruit items 
-	}
-?>
-	<?php foreach ($fruitArray as $fruit) { ?>
-		<li><?=$fruit?></li>
-	<?php } ?>
