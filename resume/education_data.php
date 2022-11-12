1ae6ece07bf53fd4f663f90c7378158543852ea0
lilian habib <lilianslku@gmail.com>
2022-11-11 18:22:13 -0800

    Add resume page

---
+++ resume/education_data.php
@@ -0,0 +1,24 @@
+
+
+<?php 
+	
+	$education_data = [
+
+		[
+			"school" => "Perpetual Education - Design for the Web", 
+			"year" => "June. 2022 - January 2023",
+		],
+
+		[
+			"school" => "Ontario Tech University", 
+			"major" => "Bachelor of Education", 
+			"year" => "Sept. 2019 - June 2021", 
+		],
+
+		[
+			"school" => "Ryerson University", 
+			"major" => "Bachelor of Technology, Minor in Marketing", 
+			"year" => "Sept. 2013 - June 2028",
+		],
+
+	];
