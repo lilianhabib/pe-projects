e13df2967ce46d6b898129dca171155e45e5e902
lilian habib <lilianslku@gmail.com>
2022-11-30 18:48:22 -0800

    Update routing php project

--- projects/php/router-site/pages/standard.php
+++ projects/php/router-site/pages/standard.php
@@ -1,1 +1,17 @@
 <?php include ('page-header.php');?>
+
+
+<?php 
+	
+	foreach ($pageData["sections"] as $section) {
+		if ($section["module"] == "generic_text") {
+			include('partials/page-section.php');
+		}
+
+		if ($section["module"] == "things_grid") {
+			include ("partials/things-grid.php");
+		}
+		
+	}
+
+?>
