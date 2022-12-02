0b844f3beeef864d687d2118464c487aeb0d2414
lilian habib <lilianslku@gmail.com>
2022-12-01 21:22:25 -0800

    Update responsive layout garden

---
+++ projects/responsive-layout-garden/layout/layout10/index.php
@@ -0,0 +1,112 @@
+
+<html>
+
+    <meta name="author" content="Lilian Habib">
+  <head> 
+    <meta charset="utf-8">
+    <meta name="description" content="A web developer passionate about creating meaningful, responsive, user-accessible, and engaging experiences through digital means.">
+
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+  
+
+    <meta property="og:image" content="https://peprojects.dev/alpha-5/lilian/images/lilian1.png">
+    
+
+    <link rel="shortcut icon" href="images/lilian1.png" type="image/png">
+
+    <link rel='stylesheet' href='style.css'>
+    
+    <!-- font --> 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Tienne:wght@400;700;900&display=swap" rel="stylesheet">
+
+  
+    <!-- Facebook Meta Tags -->
+      <meta property="og:url" content="https://peprojects.dev/alpha-5/lilian/">
+      <meta property="og:type" content="website">
+      <meta property="og:title" content=" index ">
+      <meta property="og:description" content="A web developer passionate about creating meaningful, responsive, user-accessible, and engaging experiences through digital means.">
+      <meta property="og:image" content="https://peprojects.devimages/metalilian.png?hello">
+
+      <!-- Twitter Meta Tags -->
+      <meta name="twitter:card" content="summary_large_image">
+      <meta property="twitter:domain" content="peprojects.dev">
+      <meta property="twitter:url" content="https://peprojects.dev/alpha-5/lilian">
+      <meta name="twitter:title" content=" index ">
+      <meta name="twitter:description" content="A web developer passionate about creating meaningful, responsive, user-accessible, and engaging experiences through digital means.">
+      <meta name="twitter:image" content="https://peprojects.devimages/metalilian.png">
+
+    <title>Lilian's Responsive Layout Garden</title>
+  </head> 
+
+
+  <body>
+
+    <header>
+      
+    </header>
+    
+    <main>
+
+      <layout-overview>
+
+        <section class='header-menu'> 
+          <header>
+            
+            <picture> 
+              <img src="https://www.peprojects.dev/images/landscape.jpg">
+            </picture>
+
+            
+            <nav>
+              <?php foreach ([1,2,3,4] as $item) {?>
+
+                <a href="#">Menu Item</a>
+
+              <?php }?>
+            </nav> 
+
+            <nav2>
+              <div class="line"></div>
+              <div class="line"></div>
+              <div class="line"></div>
+            </nav2>
+
+          </header>
+        </section>
+
+
+        <section class='site-intro'> 
+
+          <h1 class='loud-voice'>the mobile <br> innovation agency</h1>
+
+          <a href="#">Contact Us</a>
+     
+        </section>
+
+
+        <section class="site-description"> 
+          <p class="calm-voice">Creating beautiful digital products engineered to drive growth for the world's leading brands.</p>
+        </section>
+
+
+        <section class='logo-grid'>
+
+
+          <div class='logo-list'> 
+            <?php foreach ([1,2,3,4,5,6,7,8,9,10] as $picture){?>
+
+              <picture> 
+                <img src="https://www.peprojects.dev/images/square.jpg">
+              </picture>
+
+            <?php } ?> 
+          </div>
+          
+        </section>
+        
+
+      </layout-overview>
+    </main>
+  </body>
