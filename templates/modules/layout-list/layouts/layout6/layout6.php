4e1d9fa249f5a18388864966b1f4e53a479a8587
lilian habib <lilianslku@gmail.com>
2023-01-02 19:34:20 -0800

    Add layout folders and files

---
+++ templates/modules/layout-list/layouts/layout6/layout6.php
@@ -0,0 +1,130 @@
+<!doctype html> 
+
+<html>
+
+    <meta name="author" content="Lilian Habib">
+  <head> 
+    <meta charset="utf-8">
+    <meta name="description" content="A web developer passionate about creating meaningful, responsive, user-accessible, and engaging experiences through digital means.">
+
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+  
+
+    <meta property="og:image" content="https://peprojects.dev/alpha-5/lilian/images/lilian1.png">
+    
+
+    <link rel="shortcut icon" href="images/lilian1.png" type="image/png">
+
+    <link rel='stylesheet' href='style.css'>
+    
+    <!-- font --> 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Tienne:wght@400;700;900&display=swap" rel="stylesheet">
+
+  
+    <!-- Facebook Meta Tags -->
+      <meta property="og:url" content="https://peprojects.dev/alpha-5/lilian/">
+      <meta property="og:type" content="website">
+      <meta property="og:title" content=" index ">
+      <meta property="og:description" content="A web developer passionate about creating meaningful, responsive, user-accessible, and engaging experiences through digital means.">
+      <meta property="og:image" content="https://peprojects.devimages/metalilian.png?hello">
+
+      <!-- Twitter Meta Tags -->
+      <meta name="twitter:card" content="summary_large_image">
+      <meta property="twitter:domain" content="peprojects.dev">
+      <meta property="twitter:url" content="https://peprojects.dev/alpha-5/lilian">
+      <meta name="twitter:title" content=" index ">
+      <meta name="twitter:description" content="A web developer passionate about creating meaningful, responsive, user-accessible, and engaging experiences through digital means.">
+      <meta name="twitter:image" content="https://peprojects.devimages/metalilian.png">
+
+    <title>Lilian's Responsive Layout Garden</title>
+  </head> 
+
+  <body> 
+
+    <main>
+      <inner-column> 
+        <section class='page-section'> 
+
+            <div class="intro"> 
+              <h2 class="bold-voice">Heading level 2 small</h2>
+              <p class="tiny-voice">This is some body text. This is some body text. This is some body text. This is some body text.</p> 
+            </div>
+
+            <div class="detail-section"> 
+    
+            <div class="detail"> 
+              <hr>
+              <h2 class="loud-voice">Heading level 3 small</h2>
+              <p class="tiny-voice">This is some body text. This is some body text. This is some body text. This is some body text.</p> 
+            </div>
+
+            <div class="detail"> 
+              <hr>
+              <h2 class="loud-voice">Heading level 3 small</h2>
+              <p class="tiny-voice">This is some body text. This is some body text. This is some body text. This is some body text.</p> 
+            </div>
+
+            <div class="detail">
+              <hr> 
+              <h2 class="loud-voice">Heading level 3 small</h2>
+              <p class="tiny-voice">This is some body text. This is some body text. This is some body text. This is some body text.</p> 
+            </div>
+
+
+
+            <div class="detail"> 
+              <hr>
+              <h2 class="loud-voice">Heading level 3 small</h2>
+              <p class="tiny-voice">This is some body text. This is some body text. This is some body text. This is some body text.</p> 
+            </div>
+
+            <div class="detail"> 
+            <hr>
+            <h2 class="loud-voice">Heading level 3 small</h2>
+            <p class="tiny-voice">This is some body text. This is some body text. This is some body text. This is some body text.</p> 
+            </div>
+
+            <div class="detail"> 
+            <hr>
+            <h2 class="loud-voice">Heading level 3 small</h2>
+            <p class="tiny-voice">This is some body text. This is some body text. This is some body text. This is some body text.</p> 
+            </div>
+          </div>
+
+        </section> 
+
+        <section class='image-section'> 
+
+
+            <picture> 
+              <img src="https://www.peprojects.dev/images/square.jpg">
+            </picture>
+
+            <picture> 
+              <img src="https://www.peprojects.dev/images/square.jpg">
+            </picture>
+
+
+
+
+            <picture> 
+              <img src="https://www.peprojects.dev/images/square.jpg">
+            </picture>
+
+            <picture> 
+              <img src="https://www.peprojects.dev/images/square.jpg">
+            </picture>
+
+
+
+        </section>
+
+      </inner-column> 
+
+    </main> 
+  </body>
+
+
+</html>
---
+++ templates/modules/layout-list/layouts/layout9/layout9.php
@@ -0,0 +1,289 @@
+<!doctype html> 
+
+<html>
+
+    <meta name="author" content="Lilian Habib">
+  <head> 
+    <meta charset="utf-8">
+    <meta name="description" content="A web developer passionate about creating meaningful, responsive, user-accessible, and engaging experiences through digital means.">
+
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+  
+
+    <meta property="og:image" content="https://peprojects.dev/alpha-5/lilian/images/lilian1.png">
+    
+
+    <link rel="shortcut icon" href="images/lilian1.png" type="image/png">
+
+    <link rel='stylesheet' href='style.css'>
+    
+    <!-- font --> 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Tienne:wght@400;700;900&display=swap" rel="stylesheet">
+
+  
+    <!-- Facebook Meta Tags -->
+      <meta property="og:url" content="https://peprojects.dev/alpha-5/lilian/">
+      <meta property="og:type" content="website">
+      <meta property="og:title" content=" index ">
+      <meta property="og:description" content="A web developer passionate about creating meaningful, responsive, user-accessible, and engaging experiences through digital means.">
+      <meta property="og:image" content="https://peprojects.devimages/metalilian.png?hello">
+
+      <!-- Twitter Meta Tags -->
+      <meta name="twitter:card" content="summary_large_image">
+      <meta property="twitter:domain" content="peprojects.dev">
+      <meta property="twitter:url" content="https://peprojects.dev/alpha-5/lilian">
+      <meta name="twitter:title" content=" index ">
+      <meta name="twitter:description" content="A web developer passionate about creating meaningful, responsive, user-accessible, and engaging experiences through digital means.">
+      <meta name="twitter:image" content="https://peprojects.devimages/metalilian.png">
+
+    <title>Lilian's Responsive Layout Garden</title>
+  </head> 
+
+
+  <script>
+
+    const plusBtn = document.getElementById("plus")
+
+    plusBtn.addEventListen("click", function()) {
+      let details =
+    }
+
+  </script>
+
+
+
+
+  <body>
+
+    <main>
+
+      <inner-column> 
+
+        <section class="page-section"> 
+
+          <div class="intro"> 
+            <span class='little-voice'>Little Teaser</span>
+            <h2 class='loud-voice'>Heading level 2 <span class='small-voice'>small</span></h2>
+
+            <p class='intro-voice'>This is some body text. This is some body text. This is some body text. This is some body text.</p>
+          </div>
+
+
+          <div class="detail-section"> 
+
+            <text> 
+              <span class="little-voice">Little Teaser</span>
+            </text>
+
+            <details>
+
+              <summary class="bold-voice">Heading level 3 small
+
+                <svg id='plus' width="100%" height="100%" viewBox="0 0 50 50" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"><g transform="matrix(1,0,0,1,-1.62858,0)"><rect x="25.561" y="0.017" width="2.756" height="49.988"/></g><g transform="matrix(6.12323e-17,1,-1.00105,6.12964e-17,50.0984,-1.91906)"> <rect x="25.561" y="0.017" width="2.756" height="49.988"/></g></svg>
+
+                <svg id="minus" width="100%" height="100%" viewBox="0 0 50 50" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+                <g transform="matrix(6.12323e-17,1,-1.00105,6.12964e-17,50.0984,-1.91906)">
+                <rect x="25.561" y="0.017" width="2.756" height="49.988"/>
+                </g></svg>
+
+              </summary>
+
+              <p class='intro-voice'> 
+              <span aria-hidden="true"> This is some body text. This is some body text. This is some body text. This is some body text.</span>
+              </p>  
+
+            </details>
+
+           <details>
+
+              <summary class="bold-voice">Heading level 3 small
+                <svg id='plus' width="100%" height="100%" viewBox="0 0 50 50" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"><g transform="matrix(1,0,0,1,-1.62858,0)"><rect x="25.561" y="0.017" width="2.756" height="49.988"/></g><g transform="matrix(6.12323e-17,1,-1.00105,6.12964e-17,50.0984,-1.91906)"> <rect x="25.561" y="0.017" width="2.756" height="49.988"/></g></svg>
+
+                <svg id="minus" width="100%" height="100%" viewBox="0 0 50 50" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+                <g transform="matrix(6.12323e-17,1,-1.00105,6.12964e-17,50.0984,-1.91906)">
+                <rect x="25.561" y="0.017" width="2.756" height="49.988"/>
+                </g></svg>
+
+              </summary>
+
+              <p class='intro-voice'> 
+              <span aria-hidden="true"> This is some body text. This is some body text. This is some body text. This is some body text.</span>
+              </p>  
+
+            </details>
+
+           <details>
+
+              <summary class="bold-voice">Heading level 3 small
+                <svg id='plus' width="100%" height="100%" viewBox="0 0 50 50" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"><g transform="matrix(1,0,0,1,-1.62858,0)"><rect x="25.561" y="0.017" width="2.756" height="49.988"/></g><g transform="matrix(6.12323e-17,1,-1.00105,6.12964e-17,50.0984,-1.91906)"> <rect x="25.561" y="0.017" width="2.756" height="49.988"/></g></svg>
+
+                <svg id="minus" width="100%" height="100%" viewBox="0 0 50 50" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+                <g transform="matrix(6.12323e-17,1,-1.00105,6.12964e-17,50.0984,-1.91906)">
+                <rect x="25.561" y="0.017" width="2.756" height="49.988"/>
+                </g></svg>
+
+              </summary>
+
+              <p class='intro-voice'> 
+              <span aria-hidden="true"> This is some body text. This is some body text. This is some body text. This is some body text.</span>
+              </p>  
+
+            </details>
+
+            <div class="detail2-section"> 
+
+              <text> 
+                <span class="little-voice">Little Teaser</span>
+              </text>
+
+              <details>
+
+                <summary class="bold-voice">Heading level 3 small
+                  <svg id='plus' width="100%" height="100%" viewBox="0 0 50 50" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"><g transform="matrix(1,0,0,1,-1.62858,0)"><rect x="25.561" y="0.017" width="2.756" height="49.988"/></g><g transform="matrix(6.12323e-17,1,-1.00105,6.12964e-17,50.0984,-1.91906)"> <rect x="25.561" y="0.017" width="2.756" height="49.988"/></g></svg>
+
+                  <svg id="minus" width="100%" height="100%" viewBox="0 0 50 50" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+                  <g transform="matrix(6.12323e-17,1,-1.00105,6.12964e-17,50.0984,-1.91906)">
+                  <rect x="25.561" y="0.017" width="2.756" height="49.988"/>
+                  </g></svg>
+
+                </summary>
+
+                <p class='intro-voice'> 
+                <span aria-hidden="true"> This is some body text. This is some body text. This is some body text. This is some body text.</span> </p>  
+
+            </details>
+
+            <details>
+
+                <summary class="bold-voice">Heading level 3 small
+                  <svg id='plus' width="100%" height="100%" viewBox="0 0 50 50" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"><g transform="matrix(1,0,0,1,-1.62858,0)"><rect x="25.561" y="0.017" width="2.756" height="49.988"/></g><g transform="matrix(6.12323e-17,1,-1.00105,6.12964e-17,50.0984,-1.91906)"> <rect x="25.561" y="0.017" width="2.756" height="49.988"/></g></svg>
+
+                  <svg id="minus" width="100%" height="100%" viewBox="0 0 50 50" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+                  <g transform="matrix(6.12323e-17,1,-1.00105,6.12964e-17,50.0984,-1.91906)">
+                  <rect x="25.561" y="0.017" width="2.756" height="49.988"/>
+                  </g></svg>
+
+                </summary>
+
+                <p class='intro-voice'> 
+                <span aria-hidden="true"> This is some body text. This is some body text. This is some body text. This is some body text.</span> </p>  
+
+            </details>
+
+            <details>
+
+                <summary class="bold-voice">Heading level 3 small
+                  <svg id='plus' width="100%" height="100%" viewBox="0 0 50 50" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"><g transform="matrix(1,0,0,1,-1.62858,0)"><rect x="25.561" y="0.017" width="2.756" height="49.988"/></g><g transform="matrix(6.12323e-17,1,-1.00105,6.12964e-17,50.0984,-1.91906)"> <rect x="25.561" y="0.017" width="2.756" height="49.988"/></g></svg>
+
+                  <svg id="minus" width="100%" height="100%" viewBox="0 0 50 50" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+                  <g transform="matrix(6.12323e-17,1,-1.00105,6.12964e-17,50.0984,-1.91906)">
+                  <rect x="25.561" y="0.017" width="2.756" height="49.988"/>
+                  </g></svg>
+
+                </summary>
+
+                <p class='intro-voice'> 
+                <span aria-hidden="true"> This is some body text. This is some body text. This is some body text. This is some body text.</span> </p>  
+
+            </details>
+
+            <details>
+
+                <summary class="bold-voice">Heading level 3 small
+                  <svg id='plus' width="100%" height="100%" viewBox="0 0 50 50" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"><g transform="matrix(1,0,0,1,-1.62858,0)"><rect x="25.561" y="0.017" width="2.756" height="49.988"/></g><g transform="matrix(6.12323e-17,1,-1.00105,6.12964e-17,50.0984,-1.91906)"> <rect x="25.561" y="0.017" width="2.756" height="49.988"/></g></svg>
+
+                  <svg id="minus" width="100%" height="100%" viewBox="0 0 50 50" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+                  <g transform="matrix(6.12323e-17,1,-1.00105,6.12964e-17,50.0984,-1.91906)">
+                  <rect x="25.561" y="0.017" width="2.756" height="49.988"/>
+                  </g></svg>
+
+                </summary>
+
+                <p class='intro-voice'> 
+                <span aria-hidden="true"> This is some body text. This is some body text. This is some body text. This is some body text.</span> </p>  
+
+            </details>
+
+            <details>
+
+                <summary class="bold-voice">Heading level 3 small
+                  <svg id='plus' width="100%" height="100%" viewBox="0 0 50 50" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"><g transform="matrix(1,0,0,1,-1.62858,0)"><rect x="25.561" y="0.017" width="2.756" height="49.988"/></g><g transform="matrix(6.12323e-17,1,-1.00105,6.12964e-17,50.0984,-1.91906)"> <rect x="25.561" y="0.017" width="2.756" height="49.988"/></g></svg>
+
+                  <svg id="minus" width="100%" height="100%" viewBox="0 0 50 50" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+                  <g transform="matrix(6.12323e-17,1,-1.00105,6.12964e-17,50.0984,-1.91906)">
+                  <rect x="25.561" y="0.017" width="2.756" height="49.988"/>
+                  </g></svg>
+
+                </summary>
+
+                <p class='intro-voice'> 
+                <span aria-hidden="true"> This is some body text. This is some body text. This is some body text. This is some body text.</span> </p>  
+
+            </details>
+
+            <details>
+
+                <summary class="bold-voice">Heading level 3 small
+                  <svg id='plus' width="100%" height="100%" viewBox="0 0 50 50" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"><g transform="matrix(1,0,0,1,-1.62858,0)"><rect x="25.561" y="0.017" width="2.756" height="49.988"/></g><g transform="matrix(6.12323e-17,1,-1.00105,6.12964e-17,50.0984,-1.91906)"> <rect x="25.561" y="0.017" width="2.756" height="49.988"/></g></svg>
+
+                  <svg id="minus" width="100%" height="100%" viewBox="0 0 50 50" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+                  <g transform="matrix(6.12323e-17,1,-1.00105,6.12964e-17,50.0984,-1.91906)">
+                  <rect x="25.561" y="0.017" width="2.756" height="49.988"/>
+                  </g></svg>
+
+                </summary>
+
+                <p class='intro-voice'> 
+                <span aria-hidden="true"> This is some body text. This is some body text. This is some body text. This is some body text.</span> </p>  
+
+            </details>
+
+            <details>
+
+                <summary class="bold-voice">Heading level 3 small
+                  <svg id='plus' width="100%" height="100%" viewBox="0 0 50 50" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"><g transform="matrix(1,0,0,1,-1.62858,0)"><rect x="25.561" y="0.017" width="2.756" height="49.988"/></g><g transform="matrix(6.12323e-17,1,-1.00105,6.12964e-17,50.0984,-1.91906)"> <rect x="25.561" y="0.017" width="2.756" height="49.988"/></g></svg>
+
+                  <svg id="minus" width="100%" height="100%" viewBox="0 0 50 50" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+                  <g transform="matrix(6.12323e-17,1,-1.00105,6.12964e-17,50.0984,-1.91906)">
+                  <rect x="25.561" y="0.017" width="2.756" height="49.988"/>
+                  </g></svg>
+
+                </summary>
+
+                <p class='intro-voice'> 
+                <span aria-hidden="true"> This is some body text. This is some body text. This is some body text. This is some body text.</span> </p>  
+
+            </details>
+
+            <details>
+
+                <summary class="bold-voice">Heading level 3 small
+                  <svg id='plus' width="100%" height="100%" viewBox="0 0 50 50" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"><g transform="matrix(1,0,0,1,-1.62858,0)"><rect x="25.561" y="0.017" width="2.756" height="49.988"/></g><g transform="matrix(6.12323e-17,1,-1.00105,6.12964e-17,50.0984,-1.91906)"> <rect x="25.561" y="0.017" width="2.756" height="49.988"/></g></svg>
+
+                  <svg id="minus" width="100%" height="100%" viewBox="0 0 50 50" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+                  <g transform="matrix(6.12323e-17,1,-1.00105,6.12964e-17,50.0984,-1.91906)">
+                  <rect x="25.561" y="0.017" width="2.756" height="49.988"/>
+                  </g></svg>
+
+                </summary>
+
+                <p class='intro-voice'> 
+                <span aria-hidden="true"> This is some body text. This is some body text. This is some body text. This is some body text.</span> </p>  
+
+            </details>
+            </div>
+
+    
+          </div>
+
+
+        </section>
+
+      </inner-column>
+
+    </main> 
+
+  </body>
+</html>
