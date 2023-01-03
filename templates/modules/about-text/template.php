c21fbcd6eafdd4c0981ef511f2ef4419b1de76de
lilian habib <lilianslku@gmail.com>
2023-01-02 19:33:51 -0800

    Add about text module

---
+++ templates/modules/about-text/template.php
@@ -0,0 +1,26 @@
+
+
+<section class=''> 
+	<inner-column>
+
+		<about-text>
+			<div class="page-links"> 
+
+				<a href="?page=resume" class='page-link'>Resume</a>
+
+				<a href="?page=goals" class='page-link'>Goals</a>
+
+			</div>  
+
+			<text-content> 
+
+				<p class="calm-voice"><?=$section["copy"]?></p>
+			</text-content>
+
+
+		
+
+
+		</about-text>
+	</inner-column>
+</section>
