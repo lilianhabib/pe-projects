c40476fc4b73ba3ee892300db5e14c60efc9ac25
lilian habib <lilianslku@gmail.com>
2023-01-04 15:11:51 -0800

    Add metadata

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<head> 
+	<meta charset="utf-8">
+	<meta name="author" content="Lilian Habib">
+	<meta name="description" content="A web developer passionate about creating meaningful, responsive, user-accessible, and engaging experiences through digital means.">
+
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+
+
+	<meta property="og:image" content="https://peprojects.dev/alpha-5/lilian/images/lilian1.png">
+	
+
+	<link rel="shortcut icon" href="images/lilian1.png" type="image/png">
+</head>
+
 <?php include('functions.php'); ?>
 
 
