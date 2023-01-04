614b623d5fa75ab0b458136341df1455c5174bae
lilian habib <lilianslku@gmail.com>
2023-01-04 12:07:33 -0800

    Modify page links

--- templates/modules/layout-list/template.php
+++ templates/modules/layout-list/template.php
@@ -4,29 +4,33 @@
 	$layouts = json_decode($json, true); 
 ?>
 
-<section class=''>
+<section class='<?=$pageId?>'>
 	<inner-column> 
+		<div class="page-links"> 
+
+                <a href="?page=exercises" class='page-link'>Exercises for Programmers</a>
+
+                <a href="?page=projects" class='page-link'>Projects</a>
+
+        </div> 
 
 		<layout-list>
 
 				<?php foreach ($layouts as $layout) { 
 					$slug = $layout["slug"];
 					$name = $layout["name"]; 
+					$description = $layout["description"]; 
 
 					?>
 
 					<section class="layout-item">
 			
-						<h2><?=$layout["name"]?></h2>
-						<p><?=$layout["description"]?></p>
+						<p><?=$description?></p>
 						<?php include ("layouts/$name/$slug.php");?>
 						<!-- <a href="?page=layout&<?=$slug?>">Visit <?=$name?></a> -->
 					</section>
 
-
 				<?php } ?>
-
-				
 
 		</layout-list>
 
