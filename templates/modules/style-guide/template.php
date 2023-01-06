b889a0ddc36b45d400708f282e9ff0e994592d85
lilian habib <lilianslku@gmail.com>
2023-01-05 22:30:02 -0800

    Update json file path

--- templates/modules/style-guide/template.php
+++ templates/modules/style-guide/template.php
@@ -1,22 +1,153 @@
 <?php 
 
-	$json = file_get_contents('data/projects-data.json'); 
-	$projects = json_decode($json, true); 
+	$json = file_get_contents('data/style-guide-data.json'); 
+	$styleGuideData = json_decode($json, true); 
 
 	$pageId = "home"; 
 	if (isset($_GET["page"]) ){
 		$pageId = $_GET["page"]; 
 	}
+
+
 ?>
 
+<hr>
 
 <section class='<?=$pageId?>'> 
 	<inner-column>
-
-		<style-guide> 
-			
 		
+		<style-guide>
+
+
+
+			<?php foreach ($styleGuideData as $style) { ?>
+
+				<section class='data'> 
+
+					<h2 class="bold-voice"><?=$style["heading"]?></h2>
+					<hr>
+
+					<h3 class="lazy-voice"><?=$style["sub-heading"]?></h3>
+					
+					<?php foreach ($style["examples"] as $example) { ?>
+
 	
+						<?php if (isset($example["example1"]) ) { ?>
+							<h1 class='loud-voice'><?=$example["example1"]?></h1>
+						<?php } ?> 
+
+						<?php if (isset($example["blurb1"]) ) { ?>
+							<p class='lazy-voice'><?=$example["blurb1"]?></p>
+						<?php } ?> 
+
+					
+
+						<?php if (isset($example["example2"]) ) { ?>
+							<h2 class='lazy-voice'><?=$example["example2"]?></h2>
+						<?php } ?> 
+
+						<?php if (isset($example["blurb2"]) ) { ?>
+							<p class='lazy-voice'><?=$example["blurb2"]?></p>
+						<?php } ?> 
+
+						
+
+						<?php if (isset($example["example3"]) ) { ?>
+							<p class='bold-voice'><?=$example["example3"]?></p>
+						<?php } ?> 
+
+						<?php if (isset($example["blurb3"]) ) { ?>
+							<p class='bold-voice'><?=$example["blurb3"]?></p>
+						<?php } ?>
+
+				
+
+						<?php if (isset($example["example4"]) ) { ?>
+							<p class='normal-voice'><?=$example["example4"]?></p>
+						<?php } ?> 
+
+						<?php if (isset($example["blurb4"]) ) { ?>
+							<p class='normal-voice'><?=$example["blurb4"]?></p>
+						<?php } ?>
+
+				
+					
+					<?php } ?>
+						
+				</section> 
+
+			<?php } ?>
+
+
+			<section class="modules"> 
+				<h2 class="bold-voice">Modules</h2>
+				<hr>
+
+				<contact-list> 
+					<h3 class='little-voice'>About</h3>
+					<?php 
+			
+						$copy = "<p>This is the contact module. This is where the copy goes/p>";
+						include ('templates/modules/standard-text/template.php');
+
+					?>
+				</contact-list>
+
+				<h3 class='little-voice'>Projects</h3>
+
+				<?php 
+		
+					$copy = "<p>This is the default text</p>";
+					include ('templates/modules/projects-list/template.php');
+
+				?>
+
+				<h3 class='little-voice'>Exercises for Programmers</h3>
+
+				<?php 
+		
+					$copy = "<p>This is the default text</p>";
+					include ('templates/modules/exercises-list/template.php');
+
+				?>
+				<h3 class='little-voice'>Resume</h3>
+				<?php 
+		
+					$copy = "<p>This is the default text</p>";
+					include ('templates/modules/resume-list/template.php');
+
+				?>
+
+				<h3 class='little-voice'>Layouts</h3>
+
+				<?php 
+		
+					$copy = "<p>This is the default text</p>";
+					include ('templates/modules/layout-list/template.php');
+
+				?>
+
+				<h3 class='little-voice'>Goals</h3>
+
+				<?php 
+		
+					$copy = "<p>This is the default text</p>";
+					include ('templates/modules/goals-list/template.php');
+
+				?>
+
+				<h3 class='little-voice'>Contact</h3>
+
+				<?php 
+		
+					$copy = "<p>This is the default text</p>";
+					include ('templates/modules/contact-list/template.php');
+
+				?>
+
+
+			</section>
+
 		</style-guide>
 
 	</inner-column>
