60093cae58a32ab289f8c491551b6597fd90e6a4
lilian habib <lilianslku@gmail.com>
2023-01-05 22:30:19 -0800

    Add page link file

---
+++ templates/partials/page-link.php
@@ -0,0 +1,29 @@
+<?php 
+
+    if (isset($_GET["page"]) ){
+        $pageId = $_GET["page"]; 
+    }
+
+?>
+
+
+<!-- 
+ <div class="page-links">  -->
+
+    <a href="?page=resume" class='page-link' id='resume'>Resume</a>
+
+    <a href="?page=contact" class='page-link'>Contact</a>
+
+    <a href="?page=about" class='page-link'>About</a>
+
+    <a href="?page=goals" class='page-link'>Goals</a>
+
+    <a href="?page=layouts" class='page-link'>Responsive Layout Garden</a>
+
+    <a href="?page=projects" class='page-link'>Projects</a>
+
+    <a href="mailto:lilianslku@gmail.com" class='page-link'>Email</a>
+
+    <a href="https://calendly.com/liliankuhabib/30min" target="_blank" class='page-link'>Google Meet</a>
+
+<!-- </div>  -->
