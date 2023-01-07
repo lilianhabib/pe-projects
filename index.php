1002ac6799198f4242504bb222bad47a69be7373
lilian habib <lilianslku@gmail.com>
2023-01-07 11:16:25 -0800

    Add metadata

--- index.php
+++ index.php
@@ -16,8 +16,16 @@
 <html>
 	<head>
 		<meta charset="utf-8">
+		<meta name="author" content="Lilian Habib">
+		<meta name="description" content="A web developer passionate about creating meaningful, responsive, user-accessible, and engaging experiences through digital means.">
+		
+		<meta property="og:image" content="https://peprojects.dev/alpha-5/lilian/images/lilian1.png">
+		
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="styles/site.css">
+
+
+
 		<title>Lilian's Portfolio</title>
 	</head>
 
