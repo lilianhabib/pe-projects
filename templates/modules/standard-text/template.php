48e39dcf9ccae3d341400a1930724058fca71fd7
lilian habib <lilianslku@gmail.com>
2023-01-06 22:18:19 -0800

    Modify template

--- templates/modules/standard-text/template.php
+++ templates/modules/standard-text/template.php
@@ -5,50 +5,53 @@
 ?>
 
 
-<section class=''> 
+<section class='about-page'> 
 	<inner-column>
 		<div class="page-links"> 
 
 			<a href="?page=resume" class='page-link'>Resume</a>
 			<a href="?page=goals" class='page-link'>Goals</a>
 
-		</div>  
+		</div>
 
-		<about-text>
+		<about-content>   
+
+			<about-text>
+				
+
+				<?php foreach ($thatAbout as $about) {
+					$copy = $about["copy"] ?? "Default text"; 
+					$heading = $about["heading"] ?? "Default text";
+					?>
+
+					<div class='copy'> 
+
+						<?php if (isset ($about["copy"]) ) { ?>
+							<p class="normal-voice"><?=$copy?></p>
+						<?php } ?>
+				
+					</div>
+
+				<?php  } ?>
+
+				
+
+			</about-text>
+
+			<img-grid>
+				<?php foreach ($about["image"] as $img) { ?>
+
+					<picture class='about-grid'> 
+
+						<img class='about-img' src='<?=$img?>'>
+							
+					</picture>
+
+				<?php  } ?>
+			</img-grid>
 			
 
-			<?php foreach ($thatAbout as $about) {
-				$copy = $about["copy"] ?? "Default text"; 
-				$heading = $about["heading"] ?? "Default text";
-				?>
-
-				<div class='copy'> 
-
-					<?php if (isset ($about["copy"]) ) { ?>
-						<p class="normal-voice"><?=$copy?></p>
-					<?php } ?>
-			
-				</div>
-
-			<?php  } ?>
-
-			
-
-		</about-text>
-
-		<img-grid>
-			<?php foreach ($about["image"] as $img) { ?>
-
-				<picture class='about-grid'> 
-
-					<img class='about-img' src='<?=$img?>'>
-						
-				</picture>
-
-			<?php  } ?>
-		</img-grid>
-		
-
-		</about-text>
+			</about-text>
+		</about-content>
 	</inner-column>
 </section>
