bdc3124b068f00a8585654559bf99bc1f53f5388
lilian habib <lilianslku@gmail.com>
2023-01-06 22:16:27 -0800

    Add template and stylesheet to 404 folder

---
+++ templates/modules/404/template.php
@@ -0,0 +1,16 @@
+
+
+<section class='<?=$pageId?>'>
+	<inner-column> 
+
+		<error-text> 
+
+			<p class="normal-voice">Unfortunately, this page does not exist.</p> 
+			<br>
+			<p class="normal-voice"> Take a look at my <a href='?page=projects' class='error-link'>Work</a> and <a href='?page=about' class='error-link'>About</a> pages, which may have what you are looking for. If not, feel free to <a href='?page=contact' class='error-link'>connect</a> with me!"</p>
+
+
+		</error-text>
+
+	</inner-column>
+</section>
