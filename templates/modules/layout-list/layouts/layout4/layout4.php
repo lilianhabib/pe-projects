43c7e454d648c9a115eddbbdb97a7585fb5197d0
lilian habib <lilianslku@gmail.com>
2023-02-10 14:08:12 -0800

    Add layout folders

---
+++ templates/modules/layout-list/layouts/layout4/layout4.php
@@ -0,0 +1,63 @@
+<!doctype html> 
+
+<html>
+  <head>
+    <title>Lilian's Responsive Layout Garden</title>
+  </head> 
+
+  <body> 
+    <header class='site-header'>
+      <inner-column>
+        <a href='#' class='logo'>
+          <img src='https://peprojects.dev/images/square.jpg'>
+        </a>
+        <span class='company-name'>The Company Name</span>
+      </inner-column>
+
+    </header>
+
+    <main class='page-content'>
+      <section class='welcome'>
+        <inner-column>
+
+          <picture class='welcome-img'>
+            <img src="https://peprojects.dev/images/landscape.jpg">
+          </picture>
+
+          <text-content>
+            <h1 class='crazy-voice'>Heading level 1</h1>
+            <a href='#' class='welcome-link'>click here</a>
+
+            <span>Some bonus thing</span>
+          </text-content>
+
+        </inner-column>
+      </section>
+
+      <section class='about'>
+        <section class='inner-column'>
+          <div class='box1'>
+            <div class='box1a'> </div>
+            <h2 class='box1b'>Heading Here</h2>
+          </div>
+
+          <div class='box2'>
+            <div class='box2a'> </div>
+            <h2 class='box2b'>Heading Here</h2>
+          </div>
+
+        </section>
+    </main>
+
+    <footer class='site-footer'>
+      <section class='footer'>
+        <div class='inner-column'>
+          <a href='#' class='logo'>
+            <img src='https://peprojects.dev/images/square.jpg'>
+          </a>
+
+        </div>
+      </section>
+    </footer>
+  </body>
+</html>
