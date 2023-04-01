d1b5410de15aea1f6a84bd23fe0191a11d8c5d8f
lilian habib <lilianslku@gmail.com>
2023-04-01 00:54:23 -0700

    Update menu link

--- templates/partials/site-menu.php
+++ templates/partials/site-menu.php
@@ -9,9 +9,9 @@
 	<ul class="menu-list">
 
 		<li class="menu-item"><a href=" ">Home</a></li>
-		<li class="menu-item"><a href="/works">Work </a></li>
-		<li class="menu-item"><a href="/about">About</a></li>
-		<li class="menu-item"><a href="/contact">Contact</a></li>
+		<li class="menu-item"><a href="works">Work </a></li>
+		<li class="menu-item"><a href="about">About</a></li>
+		<li class="menu-item"><a href="contact">Contact</a></li>
 	<!-- 	<button class='toggle'></button> -->
 
 	</ul>
