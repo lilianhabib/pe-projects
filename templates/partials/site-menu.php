dc650aa159b91d0b2e3d8433f0a37e11d47a7602
lilian habib <lilianslku@gmail.com>
2023-04-01 00:27:30 -0700

    Update home link

--- templates/partials/site-menu.php
+++ templates/partials/site-menu.php
@@ -8,7 +8,7 @@
 
 	<ul class="menu-list">
 
-		<li class="menu-item"><a href="/">Home</a></li>
+		<li class="menu-item"><a href="">Home</a></li>
 		<li class="menu-item"><a href="/works">Work </a></li>
 		<li class="menu-item"><a href="/about">About</a></li>
 		<li class="menu-item"><a href="/contact">Contact</a></li>
