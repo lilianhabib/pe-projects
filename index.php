a74ee8c650fc20a5bd553457ab0a9e957c92541b
lilian habib <lilianslku@gmail.com>
2023-04-01 00:17:20 -0700

    Update styles and add base

--- index.php
+++ index.php
@@ -16,12 +16,13 @@
 <html>
 	<head>
 		<meta charset="utf-8">
+		<base href='<?=BASE_URL?>'/>
 		<meta name="author" content="Lilian Habib">
 		<meta name="description" content="A web developer passionate about creating meaningful, responsive, user-accessible, and engaging experiences through digital means.">
 		<meta property="og:image" content="https://peprojects.dev/alpha-5/lilian/images/sharelilian.png">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link href="styles/site.css" type="text/html" rel='stylesheet' >
-		<base href='<?=BASE_URL?>'/>
+		<link href="../styles/site.css" type="text/html" rel='stylesheet' >
+		
 		<title>Lilian's Portfolio</title>
 	</head>
 
